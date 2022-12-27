5e760c8296966aaa0ab58ef9e506f9280f6a4f97
deependujha <deependujha21@gmail.com>
2022-12-27 15:30:42 +0530

    left side done

--- main_app/components/leftSideComponents/lowerPart/ExitButton.jsx
+++ main_app/components/leftSideComponents/lowerPart/ExitButton.jsx
@@ -1,53 +1,67 @@
 import React from 'react';
 import Image from 'next/image';
-import Swal from 'sweetalert2';
 import { useDisconnect } from 'wagmi';
 import { motion } from 'framer-motion';
-import { Tooltip } from "@nextui-org/react";
-
+import { Tooltip } from '@nextui-org/react';
+import { Modal, Input, Row, Checkbox, Button, Text } from '@nextui-org/react';
+import { Router } from 'next/router';
 
 const ExitButton = () => {
+	const [visible, setVisible] = React.useState(false);
+
 	const { disconnect } = useDisconnect();
 
+	const closeHandler = () => {
+		// console.log("cancelled")
+		setVisible(false);
+	};
 	const logoutBtnClicked = async () => {
-		Swal.fire({
-			title: 'Are you sure?',
-			text: 'Do you want to logout?',
-			icon: 'warning',
-			showCancelButton: true,
-			confirmButtonColor: '#023020',
-			cancelButtonColor: '#d33',
-			confirmButtonText: 'Yes, logout!',
-		}).then((result) => {
-			if (result.isConfirmed) {
-				disconnect();
-			}
-		});
+		// console.log("you have been logged out")
+		setVisible(true);
+	};
+
+	const logoutUser = async () => {
+		disconnect();
 	};
 
 	return (
-    <div className="flex justify-center justify-items-center">
-      <motion.div
-        whileHover={{ scale: 0.9 }}
-        transition={{ type: "spring", stiffness: 400, damping: 10 }}
-      >
-        <Tooltip
-          content={"logout"}
-          placement="rightStart"
-          color="error"
-        >
-          <button onClick={logoutBtnClicked}>
-            <Image
-              src="/icons/logout.png"
-              width={45}
-              height={45}
-              alt="account"
-            />
-          </button>
-        </Tooltip>
-      </motion.div>
-    </div>
-  );
+		<div className="flex justify-center justify-items-center">
+			<motion.div
+				whileHover={{ scale: 0.9 }}
+				transition={{ type: 'spring', stiffness: 400, damping: 10 }}
+			>
+				<Tooltip content={'logout'} placement="rightStart" color="error">
+					<button onClick={logoutBtnClicked}>
+						<Image
+							src="/icons/logout.png"
+							width={45}
+							height={45}
+							alt="account"
+						/>
+					</button>
+				</Tooltip>
+			</motion.div>
+			<Modal closeButton blur open={visible} onClose={closeHandler}>
+				<Modal.Header>
+					<div className="font-bold text-2xl underline text-red-600">
+						logout
+					</div>
+				</Modal.Header>
+				
+				<Modal.Body>
+					<div className="text-lg text-center">Do you want to logout?</div>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button auto flat onClick={closeHandler}>
+						cancel
+					</Button>
+					<Button auto color="error" onClick={logoutUser}>
+						logout
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</div>
+	);
 };
 
 export default ExitButton;
