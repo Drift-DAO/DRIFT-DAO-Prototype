d8f2c2f78040f272c934cec804b18926bfe6998d
deependujha <deependujha21@gmail.com>
2022-12-31 13:59:44 +0530

    changed mongo connection from local to atlas

--- backend/database/conn.js
+++ backend/database/conn.js
@@ -1,9 +1,14 @@
+import 'dotenv/config';
 import mongoose from 'mongoose';
 
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
 mongoose.set('strictQuery', true);
 
 mongoose
-	.connect('mongodb://localhost:27017/drift_dao')
+	.connect(
+		`mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.wys8pfj.mongodb.net/?retryWrites=true&w=majority`
+	)
 	.then(() => {
 		// console.log(`connected to mongodb`);
 	})
